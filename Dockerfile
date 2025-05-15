1b8c888220f50ae4125d591ec4be08bc41692d1e
lloyet <contact@lloyet.dev>
2025-05-15 11:47:40 +0200

    feat(ci/cd): add auto deploy

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM node:22.15-alpine3.21
+
+LABEL io.github.username="lloyet"
+
+WORKDIR /app
+
+COPY package.json package-lock.json /app/
+
+RUN npm ci
+
+COPY . .
+
+RUN npm run build
+
+CMD ["node", "/app/build/index.js"]
+
--- src/env/index.ts
+++ src/env/index.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
-export const HOST = process.env.HOST ?? "localhost";
+export const HOST = process.env.HOST ?? "0.0.0.0";
 export const PORT = parseInt(process.env.PORT ?? "3000");
 export const RCON_HOST = process.env.RCON_HOST ?? "localhost";
 export const RCON_PORT = parseInt(process.env.RCON_PORT ?? "25575");
